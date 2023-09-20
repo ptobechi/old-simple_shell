81b0cb768910281a103c4b8e7d6242e84bae1ad5
Tobechukwu <ptobechii@gmail.com>
2023-09-20 06:13:04 -0500

    get token length

--- get_token_len.c
+++ get_token_len.c
@@ -12,23 +12,21 @@ int get_token_len(char *str, char delim)
 	int i, j, count = 1;
 
 	if (str == NULL)
-		return (-1);
-
-	if (str[0] == '\0')
 		return (0);
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*str != '\0')
 	{
-		if (str[i] == delim)
-		{
-			for (j = i + 1; str[j] != '\0'; j++)
-				if (str[j] != delim)
-				{
-					count++;
-					i = j;
-					break;
-				}
-		}
+		if (*str == delim)
+			count++;
+		str++;
 	}
 	return (count);
+}
+int main(void)
+{
+	int res;
+	res = get_token_len("Hello How are u World", ' ');
+
+	printf("%d\n", res);
+	return (0);
 }
